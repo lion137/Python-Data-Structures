7b5dccc4194bcfcca40754c7c69c3cda9682b7f8
lion137 <lion137@gmail.com>
2016-12-11 19:41:13 +0000

    Update cons_list.py

--- cons_list.py
+++ cons_list.py
@@ -56,9 +56,16 @@ class List(metaclass=ABCMeta):
 List.register(Nil);
 List.register(Cons)
 
-
-def nth(n, xs):
-    """Returns nt-h (0 based indexing) elemt of the list,
+def concat(xs, ys): 
+    """Concatenates two lists"""
+    if xs is Nil:
+        return ys
+    else:
+        return cons(xs.head, concat(xs.tail, ys)) 
+    
+    
+def nth(n , xs):
+    """Returns nt-h (0 based indexing) elemt of the list, 
     throws an exception when out of range"""
     if xs is Nil:
         return Exception("Out Of Bound")
@@ -67,21 +74,25 @@ def nth(n, xs):
     else:
         return nth(n - 1, xs.tail)
 
-
 def length(xs):
     """Returns length of a list O(n)"""
     if xs is Nil:
         return 0
     else:
         return 1 + length(xs.tail)
+def empty(xs):
+    if xs is Nil:
+        return True
+    else:
+        return False
 
-
-def cons(elem, xs):
+def cons(elem ,xs):
     """Cons element elem to the list"""
     return Cons(elem, xs)
 
 
-def subs_in_expr(new, old, xs):  # this helper function works put single list element which maybe list itself
+#------ helper function:---------------
+def subs_in_expr(new, old, xs): # this helper function works put single list element which maybe list itself
     if xs is not Cons:
         if xs == old:
             return new
@@ -89,37 +100,32 @@ def subs_in_expr(new, old, xs):  # this helper function works put single list el
             return xs
     else:
         return subst(new, old, xs)
-
-
-def subst(new, old, xs):  # substitutes new as old in a list(possible nested)
+    
+def subst(new, old, xs):
+    """substitutes new as old in a list(possible nested)"""
     if xs is Nil:
         return Nil
     else:
-        return cons(subs_in_expr(new, old, xs.head), subst(new, old, xs.tail))
-
+        return cons(subs_in_expr(new, old, xs.head) , subst(new ,old, xs.tail))
 
 def Create_list(args):
     """Crates immutable list from any iterable args"""
-    if len(args) == 0:
+    if args is None:
         return Nil()
     tmp = len(args) - 1
-
     def helper(xs, cnt, limit):
         if cnt > limit:
             return Nil
         else:
-            return cons(xs[cnt], helper(xs, cnt + 1, limit))
-
+            return Cons(xs[cnt], helper(xs, cnt + 1, limit))
     return helper(args, 0, tmp)
-
 
 def duplicate(n, xs):
     """return n duplicates n times expression xs"""
     if n == 0:
         return Nil
     else:
-        return xs | c | duplicate(n - 1, xs)
-
+        return cons(xs, duplicate(n - 1, xs))
 
 def to_list(*args):
     """Create a single element cons list from an argument/s"""
@@ -127,3 +133,114 @@ def to_list(*args):
         return Nil()
     else:
         return Cons(args, Nil)
+
+
+def my_map(f, xs): # my_map to not cover map from std
+    """Standart map, map a function onto a list"""
+    if xs is Nil:
+        return Nil
+    else:
+        return f(xs.head) |c| my_map(f, xs.tail)
+
+def my_filter(pred, xs): # to not cover filter from std
+        """Filter on list"""
+        if xs is Nil:
+            #pdb.set_trace()
+            return Nil
+        else:
+            if pred(xs.head):
+                return cons(xs.head, my_filter(pred, xs.tail))
+            else:
+                return my_filter(pred, xs.tail)
+            
+def remove_nth(n, xs):
+    """Removes the n-th elem from the list, throw an Exception
+    if index out of bounds"""
+    if isinstance(xs, Nil):
+        return Exception("Out of Bounds")
+    else:
+        if n == 0:
+            if xs.tail is not Nil:
+                return cons(xs.tail.head, xs.tail.tail)
+            else:
+                return Nil
+        else:
+            return cons([xs.head, remove_nth(n - 1, xs.tail)])
+        
+def down(xs):
+    """Wrap parenthesis every each top level expression"""
+    return my_map(lambda x: Cons(x, Nil), xs)
+
+
+
+def list_set(xs, n, elem):
+    """Returns list with n - th element updated to elem"""
+    if isinstance(xs, Nil):
+        return Exception("Out of Bounds")
+    else:
+        if n == 0:
+            return cons(elem, xs.tail)
+        else:
+            return cons(xs.head, list_set(xs.tail, n - 1, elem))
+        
+
+#-------helper function:-----------------------
+def count_occurencies_nested(x, xs):
+    """ cont_ocurencies helper, returns number of occurencies in an expression"""
+    if not isinstance(xs, Cons):
+        if xs == x:
+            return 1
+        else:
+            return 0
+    else:
+        return count_occurencies(x, xs)
+    
+def count_occurencies(x, xs):
+    """Count occurencies of x in xs (possible nested)"""
+    if xs is Nil:
+        return 0
+    else:
+        return count_occurencies_nested(x, xs.head) + count_occurencies(x, xs.tail)
+    
+#----------helper function:-------------
+def product_helper(x, ys):
+    """Building a list of pairs - multiplies elem x times list ys """
+    if ys is Nil:
+        return Nil
+    else:
+        return cons(Cons(x, ys.head), product_helper(x, ys.tail))
+
+    
+def product(xs, ys):
+    """Returns cartesian product of two lists (lists are without
+    repetitions)."""
+    if xs is Nil:
+        return Nil
+    else:
+        return cons(product_helper(xs.head, ys), product(xs.tail, ys))
+
+def reverse(xs, ys):
+    """reverse a list"""
+    if empty(ys):
+        return xs
+    else:
+        return reverse(cons(ys.head, xs), ys.tail)
+
+def reduce(f, ys, start):
+        """Reduce left, start is the neutral element of f"""
+        def helper(f, xs, acc):
+            if xs is Nil:
+                return acc
+            else:
+                return helper(f, xs.tail, f(acc, xs.head))
+        return helper(f, ys, start)
+
+def insert(elem, n, xs):
+    """Inserts elem in a position after index n"""
+    if xs is Nil:
+        return Exception("Out of bounds")
+    else:
+        if n == 0:
+            return cons(xs.head, Cons(elem, xs.tail))
+        else:
+            return cons(xs.head, insert(elem, n - 1, xs.tail))
